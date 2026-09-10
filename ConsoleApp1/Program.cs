c57d382169267308632902f37e71b581ea2110f7
gleb4ik <keshenka35@gmail.com>
2026-09-10 23:24:07 +0300

    ввод количества операций

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            int count = ReadOperationsCount();
+
+            string[] names = new string[count];
+            double[] amounts = new double[count];
+        }
+
+        static int ReadOperationsCount()
+        {
+            int count;
+            bool isValid;
+
+            do
+            {
+                Console.Write("Введите количество операций (от 2 до 40): ");
+                string input = Console.ReadLine();
+
+                isValid = int.TryParse(input, out count) && count >= 2 && count <= 40;
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Некорректное значение. Введите целое число от 2 до 40.");
+                }
+            }
+            while (!isValid);
+
+            return count;
         }
     }
 }
